1f5ce887a87bc1ce39a477dd4d04e573793d8959
Joshua <joxuecont@gmail.com>
2025-07-07 17:34:51 -0400

    Fix CORS configuration by removing trailing slash from whitelisted origin.

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from "cors";
 
 const WHITELISTED_ORIGINS = [
   "http://localhost:5173",
-  "https://patientor-neon.vercel.app/",
+  "https://patientor-neon.vercel.app",
   "https://vercel.com/josuee-contreras-projects/patientor/Eu8zSWnHJHRTkGx1zAfRhhLp2Ltq",
 ];
 
